eb7bd8b7aa2a81699e178ef7c8eea9b2308042fd
Glenn <qianfan2@illinois.edu>
2018-11-30 17:37:44 -0600

    hullo

--- app/src/main/java/com/example/qianfan/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/qianfan/cs125finalproject/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * YOOOOOOOOOOOO
+     * @param savedInstanceState idk
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
