4356001eb38516f2a16d6c3b9fd349ec4e8543e8
Glenn <qianfan2@illinois.edu>
2018-12-04 18:25:11 -0600

    Has a button, button prints to console rn

--- app/src/main/java/com/example/qianfan/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/qianfan/cs125finalproject/MainActivity.java
@@ -2,15 +2,22 @@ package com.example.qianfan.cs125finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    /**
-     * YOOOOOOOOOOOO
-     * @param savedInstanceState idk
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.convertASCII);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("CS125FinalProject", "Will convert image to ASCII.");
+            }
+        });
     }
 }
