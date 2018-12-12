0a0d0e8485f412fa16067316ad9e229cab77a46e
andyd <tungmd2@illinois.edu>
2018-12-11 18:33:41 -0600

    improving ascii quality

--- app/src/main/java/com/example/qianfan/cs125finalproject/ImageTransform.java
+++ app/src/main/java/com/example/qianfan/cs125finalproject/ImageTransform.java
@@ -1,12 +1,12 @@
 package com.example.qianfan.cs125finalproject;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.text.Spannable;
 import android.text.style.RelativeSizeSpan;
+import android.text.style.ScaleXSpan;
+import android.text.style.TypefaceSpan;
 import android.util.Log;
-import android.widget.TextView;
 
 import com.bachors.img2ascii.Img2Ascii;
 
@@ -19,8 +19,6 @@ public class ImageTransform {
     }
 
     public void convert() {
-        //Bitmap image = BitmapFactory.decodeFile(filename);
-
         Bitmap bm=((BitmapDrawable)activity.getImageViewer().getDrawable()).getBitmap();
 
         new Img2Ascii()
@@ -35,7 +33,9 @@ public class ImageTransform {
 
                     @Override
                     public void onResponse(Spannable text) {
+                        text.setSpan(new ScaleXSpan(2f), 0, text.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
                         text.setSpan(new RelativeSizeSpan(0.11f), 0, text.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        text.setSpan(new TypefaceSpan("monospace"), 0, text.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
                         activity.getTextView().setText(text);
                         Log.i("CS125FinalProject", text.toString());
                     }
