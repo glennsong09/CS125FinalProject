5b6e7dc7813df473041e8a3ff90890268e047758
Glenn <qianfan2@illinois.edu>
2018-12-11 22:41:20 -0600

    4 buttons, convertascii, savepicture, opencamera, accessimages; button opens camera and also puts image on imageview, save might be working? nope they dont work but here it is

--- app/src/main/java/com/example/qianfan/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/qianfan/cs125finalproject/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.qianfan.cs125finalproject;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
@@ -14,17 +17,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int CAMERA_PIC_REQUEST = 1888;
+    private static int RESULT_LOAD_IMAGE = 1;
 
     public ImageView getImageViewer() {
         return imageViewer;
@@ -53,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
                 imageViewer.setImageResource(android.R.color.transparent);
             }
         });
-
+        /**
         Button btnSI = (Button) findViewById(R.id.savePicture);
         btnSI.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,10 +67,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("CS125FinalProject", "Will save the converted picture.");
                 BitmapDrawable drawable = (BitmapDrawable) imageViewer.getDrawable();
                 Bitmap bitmap = drawable.getBitmap();
-                /**
-                File sdCardDirectory = Environment.getExternalStorageDirectory();
-                File image = new File(sdCardDirectory, "download.png");
-                */
                 File image = new File(v.getContext().getExternalCacheDir(),"test.png");
                 boolean success = false;
                 FileOutputStream outStream;
@@ -81,24 +83,59 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+         **/
 
         Button btnOC = (Button) findViewById(R.id.openCamera);
         imageViewer = (ImageView) findViewById(R.id.imageView01);
         textView = (TextView) findViewById(R.id.textView);
-
+        /**
         Button btnAI = (Button) findViewById(R.id.accessImages);
         btnAI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i("CS125FinalProject", "Will access saved images.");
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+                startActivityForResult(intent, RESULT_LOAD_IMAGE);
             }
         });
+         **/
     }
 
     public void OpenCamera(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, CAMERA_PIC_REQUEST);
     }
+    /**
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+    private static File getOutputMediaFile(int type) {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_" + timeStamp + ".jpg");
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_" + timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+     */
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -108,7 +145,18 @@ public class MainActivity extends AppCompatActivity {
             imageViewer.setImageBitmap(bitmap);
             textView.setText("");
         }
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            imageViewer.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
     }
+
 
     public Context getContext() {
         return MainActivity.this;
